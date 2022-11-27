e7daebfb1e11903dd60e2e1fb5813b0fdbbcdcfd
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-11-27 20:28:42 +0100

    fix broken analytics addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import { Analytics } from '@vercel/analytics/react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return 
-  <>
-  <Component {...pageProps} />
-  <Analytics />
-  </>
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
